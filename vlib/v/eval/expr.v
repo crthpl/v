dfa862e5ba06509d4afca0aa5852c0f4584e0711
Theoo3 <theor17@madronaschool.com>
2021-05-03 20:42:24 -0700

    [backup]

--- vlib/v/eval/expr.v
+++ vlib/v/eval/expr.v
@@ -12,27 +12,39 @@ pub fn (mut e Eval) expr(expr ast.Expr, expecting table.Type) Object {
 			// println(expr.is_method)
 			// is_method := expr.left.type_name() != 'unknown v.ast.Expr'
 			// println(is_method)
+<<<<<<< Updated upstream
+=======
 			if expr.name == 'int' {
 				e.error('methods not supported')
 			}
+>>>>>>> Stashed changes
 			mut args := expr.args.map(e.expr(it.expr, it.typ))
 			if expr.is_method {
 				args.prepend(e.expr(expr.left, expr.receiver_type))
 			}
 			match expr.language {
 				.c {
+					if expr.is_method {
+						e.error('c does not have methods')
+					}
 					match expr.name.all_after('C.') {
 						'write' {
-							return Int{C.write((args[0] as Int).val, args[1] as voidptr,
-								(args[2] as Int).val), 32}
+							return Int{C.write(args[0].int_val(), args[1] as voidptr,
+								args[2].int_val()), 32}
 						}
 						'calloc' {
-							return Object(vcalloc(int((args[0] as Int).val * (args[1] as Int).val)))
+							return Object(vcalloc(int(args[0].int_val() * args[1].int_val())))
 						}
 						'getcwd' {
 							unsafe {
-								return Charptr(&i8(&char(C.getcwd(charptr(&char(args[0] as Charptr)),
-									(args[1] as Int).val))))
+								return Charptr(&char(C.getcwd(charptr(&char(args[0] as Charptr)),
+									args[1].int_val())))
+							}
+						}
+						'memcpy' {
+							unsafe {
+								return Object(voidptr(C.memcpy(args[0].voidptr_val(),
+									args[1].voidptr_val(), args[2].int_val())))
 							}
 						}
 						// 'printf' {
@@ -67,7 +79,11 @@ pub fn (mut e Eval) expr(expr ast.Expr, expecting table.Type) Object {
 					mut func := e.mods[mod][name] or { e.mods['builtin'][name] }
 					if func is ast.FnDecl {
 						e.run_func(func as ast.FnDecl, ...args)
-						return e.return_values
+						if e.return_values.len == 1 {
+							return e.return_values[0]
+						} else {
+							return e.return_values
+						}
 					}
 					e.error('unknown function: ${mod}.$name at line $expr.pos.line_nr')
 				}
@@ -106,28 +122,32 @@ pub fn (mut e Eval) expr(expr ast.Expr, expecting table.Type) Object {
 					if expr.has_else && i + 1 == expr.branches.len { // else branch
 						do_if = true
 					} else {
-						match (branch.cond as ast.Ident).name {
-							'windows' {
-								do_if = e.pref.os == .windows
+						if branch.cond is ast.Ident {
+							match branch.cond.name {
+								'windows' {
+									do_if = e.pref.os == .windows
+								}
+								'macos' {
+									do_if = e.pref.os == .macos
+								}
+								'linux' {
+									do_if = e.pref.os == .linux
+								}
+								'android' {
+									do_if = e.pref.os == .android
+								}
+								'freebsd' {
+									do_if = e.pref.os == .freebsd
+								}
+								'prealloc' {
+									do_if = e.pref.prealloc
+								}
+								else {
+									e.error('unknown compile time if: $branch.cond.name')
+								}
 							}
-							'macos' {
-								do_if = e.pref.os == .macos
-							}
-							'linux' {
-								do_if = e.pref.os == .linux
-							}
-							'android' {
-								do_if = e.pref.os == .android
-							}
-							'freebsd' {
-								do_if = e.pref.os == .freebsd
-							}
-							'prealloc' {
-								do_if = e.pref.prealloc
-							}
-							else {
-								e.error('unknown compile time if: ${(branch.cond as ast.Ident).name}')
-							}
+						} else if branch.cond is ast.PostfixExpr {
+							do_if = (branch.cond.expr as ast.Ident).name in e.pref.compile_defines
 						}
 					}
 					if do_if {
@@ -162,14 +182,11 @@ pub fn (mut e Eval) expr(expr ast.Expr, expecting table.Type) Object {
 			return e.infix_expr(left, right, expr.op, expecting)
 		}
 		ast.IntegerLiteral {
-			if expecting in table.unsigned_integer_type_idxs {
-				return Uint{strconv.parse_uint(expr.val, 0, 64), i8(e.type_to_size(expecting))}
-			} else {
-				return Int{strconv.parse_int(expr.val, 0, 64), i8(e.type_to_size(expecting))}
-			}
+			// return u64(strconv.parse_uint(expr.val, 0, 64)
+			return i64(strconv.parse_int(expr.val, 0, 64)) // TODO: numbers larger than 2^63 (for u64)
 		}
 		ast.FloatLiteral {
-			return Float{strconv.atof64(expr.val), i8(e.type_to_size(expecting))}
+			return f64(strconv.atof64(expr.val))
 		}
 		ast.BoolLiteral {
 			return expr.val
@@ -193,9 +210,6 @@ pub fn (mut e Eval) expr(expr ast.Expr, expecting table.Type) Object {
 			}
 		}
 		ast.CastExpr {
-			if e.cur_mod == 'main' {
-				println('dsaiu')
-			}
 			x := e.expr(expr.expr, expr.expr_type)
 			if expr.typ in table.signed_integer_type_idxs {
 				match x {
@@ -215,6 +229,32 @@ pub fn (mut e Eval) expr(expr ast.Expr, expecting table.Type) Object {
 						return Int{
 							val: i64(x.val)
 							size: i8(e.type_to_size(expr.typ))
+						}
+					}
+					i64 {
+						if expecting in table.signed_integer_type_idxs {
+							return Int{
+								val: x
+								size: i8(e.type_to_size(expecting))
+							}
+						} else {
+							return Uint{
+								val: u64(x)
+								size: i8(e.type_to_size(expecting))
+							}
+						}
+					}
+					f64 {
+						if expecting in table.signed_integer_type_idxs {
+							return Int{
+								val: i64(x)
+								size: i8(e.type_to_size(expecting))
+							}
+						} else {
+							return Uint{
+								val: u64(x)
+								size: i8(e.type_to_size(expecting))
+							}
 						}
 					}
 					else {
@@ -241,6 +281,25 @@ pub fn (mut e Eval) expr(expr ast.Expr, expecting table.Type) Object {
 							size: i8(e.type_to_size(expr.typ))
 						}
 					}
+					i64 {
+						return Uint{
+							val: u64(x)
+							size: i8(e.type_to_size(expr.typ))
+						}
+					}
+					f64 {
+						if expecting in table.signed_integer_type_idxs {
+							return Int{
+								val: i64(x)
+								size: i8(e.type_to_size(expecting))
+							}
+						} else {
+							return Uint{
+								val: u64(x)
+								size: i8(e.type_to_size(expecting))
+							}
+						}
+					}
 					else {
 						e.error('unknown cast: ${e.table.get_type_symbol(expr.expr_type).str()} to ${e.table.get_type_symbol(expr.typ).str()}')
 					}
@@ -262,6 +321,12 @@ pub fn (mut e Eval) expr(expr ast.Expr, expecting table.Type) Object {
 					Float {
 						return Float{
 							val: f64(x.val)
+							size: i8(e.type_to_size(expr.typ))
+						}
+					}
+					f64 {
+						return Float{
+							val: x
 							size: i8(e.type_to_size(expr.typ))
 						}
 					}
@@ -294,7 +359,6 @@ pub fn (mut e Eval) expr(expr ast.Expr, expecting table.Type) Object {
 							return Object(Charptr(&i8(x)))
 						}
 						else {
-							println(e.local_vars['buf'])
 							e.error('unknown cast: ${e.table.get_type_symbol(expr.expr_type).str()} to ${e.table.get_type_symbol(expr.typ).str()}')
 						}
 					}
@@ -409,6 +473,7 @@ pub fn (mut e Eval) expr(expr ast.Expr, expecting table.Type) Object {
 				}
 				else {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 					e.error('unhandled prefix expression $expr.op')
 				}
 			}
@@ -431,6 +496,22 @@ pub fn (mut e Eval) expr(expr ast.Expr, expecting table.Type) Object {
 				}
 			}
 		}
+		ast.StringInterLiteral {
+			mut res := expr.vals[0]
+
+			for i, exp in expr.exprs {
+				res += e.expr(exp, expr.expr_types[i]).string()
+				res += expr.vals[i + 1]
+			}
+
+			return res
+		}
+=======
+					panic(expr.op)
+				}
+			}
+		}
+>>>>>>> Stashed changes
 		else {
 			e.error('unhandled expression $expr.type_name()')
 		}
@@ -473,6 +554,9 @@ fn (e Eval) get_escape(r rune) rune {
 		}
 		`n` {
 			`\n`
+		}
+		`0` {
+			`\0`
 		}
 		else {
 			`e`
